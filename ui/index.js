c48899f733650ecd0102422e197856b2ad4c268c
Dror Tirosh <dror@opengsn.org>
2020-05-19 19:21:23 +0300

    Added UI log

--- ui/index.js
+++ ui/index.js
@@ -27,18 +27,28 @@ async function contractCall () {
   console.log(`Mined in block: ${receipt.blockNumber}`)
 }
 
+let logview
+function log(message) {
+    message = message.replace( /(0x\w\w\w\w)\w*(\w\w\w\w)\b/g, '<b>$1...$2</b>')
+    if ( !logview) {
+        logview = document.getElementById('logview')
+    }
+    logview.innerHTML = message+"<br>\n"+logview.innerHTML
+}
 async function listenToEvents () {
   const contract = new ethers.Contract(
     contractAddress, contractAbi, provider)
 
-  contract.on('FlagCaptured', (previousHolder, currentHolder, rawEvent) =>
+  contract.on('FlagCaptured', (previousHolder, currentHolder, rawEvent) => {
+    log(`Flag Captured from&nbsp;${previousHolder} by&nbsp;${currentHolder}`)
     console.log(`Flag Captured from ${previousHolder} by ${currentHolder}`)
-  )
+  })
 }
 
 window.app = {
   contractCall,
   listenToEvents,
+  log,
   identifyNetwork
 }
 
