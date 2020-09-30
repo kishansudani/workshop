db309e03763201beb2e0cb58e9d7db5177046b33
Alex Forshtat <forshtat1@gmail.com>
2020-09-30 14:39:11 +0300

    Get back to using Ganache with ChainID set

--- docker_geth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-docker run -ti --rm --name geth -p 8545:8545 ethereum/client-go:release-1.9 \
-  --nousb --miner.gastarget 10000000 \
-  --http --http.vhosts '*,localhost,host.docker.internal' \
-  --http.corsdomain '*' --http.addr "0.0.0.0" \
-  --dev --nodiscover --maxpeers 0 --mine --miner.threads 1 --verbosity 3 \
-  --networkid 1337
