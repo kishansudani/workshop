fc74b45f450ef9091be2c1a36e4bf075811a67d1
kishansudani <kishansudani661@gmail.com>
2021-04-15 12:14:11 +0000

    Minor change

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -8,7 +8,8 @@ contract Migrations {
   uint public last_completed_migration;
 
   modifier restricted() {
-    if (msg.sender == owner) _;
+    require(msg.sender == owner);
+     _;
   }
 
   constructor() public {
